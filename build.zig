380024056004967d5b4ef32e9cbc71ad83c5a47a
Chadwain Holness <chadwainholness@gmail.com>
2023-03-18 12:07:01 -0400

    Force the use of the stage1 compiler

--- build.zig
+++ build.zig
@@ -17,11 +17,13 @@ pub fn build(builder: *std.build.Builder) void {
     const lib = builder.addStaticLibrary("rem", "rem.zig");
     lib.setBuildMode(mode);
     lib.setTarget(target);
+    lib.use_stage1 = true;
     lib.install();
 
     const lib_tests = builder.addTest("rem.zig");
     lib_tests.setBuildMode(mode);
     lib_tests.setTarget(target);
+    lib_tests.use_stage1 = true;
     const lib_tests_step = builder.step("test", "Run library tests");
     lib_tests_step.dependOn(&lib_tests.step);
 
@@ -29,6 +31,7 @@ pub fn build(builder: *std.build.Builder) void {
     html5lib_tokenizer_tests.setBuildMode(mode);
     html5lib_tokenizer_tests.setTarget(target);
     html5lib_tokenizer_tests.addPackage(rem_pkg);
+    html5lib_tokenizer_tests.use_stage1 = true;
     const html5lib_tokenizer_tests_step = builder.step("test-tokenizer", "Run tokenizer tests from html5lib-tests");
     html5lib_tokenizer_tests_step.dependOn(&html5lib_tokenizer_tests.step);
 
@@ -36,6 +39,7 @@ pub fn build(builder: *std.build.Builder) void {
     html5lib_tree_construction_tests.setBuildMode(mode);
     html5lib_tree_construction_tests.setTarget(target);
     html5lib_tree_construction_tests.addPackage(rem_pkg);
+    html5lib_tree_construction_tests.use_stage1 = true;
     const html5lib_tree_construction_tests_step = builder.step("test-tree-construction", "Run tree construction tests from html5lib-tests");
     html5lib_tree_construction_tests_step.dependOn(&html5lib_tree_construction_tests.step);
 
@@ -43,6 +47,7 @@ pub fn build(builder: *std.build.Builder) void {
     example.setBuildMode(mode);
     example.setTarget(target);
     example.addPackage(rem_pkg);
+    example.use_stage1 = true;
     example.install();
     const example_run = example.run();
     const example_step = builder.step("example", "Run an example program");
